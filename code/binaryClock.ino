d87b29c8d611fb78526ca413fe7dab882f901e0f
YunPony <aazz35995@gmail.com>
2017-06-17 17:37:51 +0800

    Update binaryClock.ino

--- code/binaryClock.ino
+++ code/binaryClock.ino
@@ -1,1 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <Timer.h>
+
+int latch = 8;
+int clock = 12;
+int data = 11;
+byte led_num=1;
+void setup(){
+	pinMode(latch, OUTPUT);
+	pinMode(clock, OUTPUT);
+	pinMode(data, OUTPUT);
+}
+void loop(){
+	byte led=0;
+	led_num=1<< led;
+  	digitalWrite(latchPin, LOW);
+
+}
